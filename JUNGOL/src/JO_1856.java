939b980ca4b94f8fab6e4c39dc66a9ff1b90f1fc
Henry <jj_jjong@naver.com>
2022-06-15 00:58:00 +0900

    feat update JO_1856

---
+++ JUNGOL/src/JO_1856.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JO_1856 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("sample/Sample_1303.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (i % 2 == 0) {
+					sb.append(i * m + j).append(" ");
+				} else {
+					sb.append((i + 1) * m - j + 1).append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
