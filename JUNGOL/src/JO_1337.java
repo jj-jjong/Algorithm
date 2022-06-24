32fe0809e61a3712b7caecf67197515d42764a26
Henry <jj_jjong@naver.com>
2022-06-24 22:19:51 +0900

    feat update JO_1337

---
+++ JUNGOL/src/JO_1337.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class JO_1337 {
+	public static void main(String[] args) throws Exception	{
+		System.setIn(new FileInputStream("sample/Sample_1337.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][N];
+		
+		int cont = 0;
+		for (int i = 0; i < N; i += 2) {
+			// 빗면
+			for (int j = i; j < N - 1 - i / 2; j++) {
+				arr[j][j - i / 2] = cont++%10;
+			}
+			// 수평선
+			for (int j = N - 1 - i; j > i / 2; j--) {
+				arr[N - i / 2 - 1][j] = cont++%10;
+			}
+			// 수직선
+			for (int j = N - 1 - i / 2; j > i; j--) {
+				arr[j][i / 2] = cont++%10;
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
